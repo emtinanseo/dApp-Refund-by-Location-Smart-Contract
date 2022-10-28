1fd8a1d0c0227bf724052cf12b207171f7e41099
emtinanseo <emtinan.s.e.osman.com>
2022-10-28 18:07:45 +0200

    modified deployment script

--- scripts/basic-deploy-interact.py
+++ scripts/basic-deploy-interact.py
@@ -1,9 +1,9 @@
 from brownie import BasicContract , accounts
 
 def main(): 
-    # Fetch the account 
+    # Fetch an account from accounts provided by Ganache CLI
     account = accounts[0] 
-    # Deploy contract 
+    # Deploy contract using deploy function of the contractContainer object (BasicContact) 
     deploy_contract = BasicContract.deploy({"from":account}) 
-    # Print contract address 
+    # Print contract address (deploy_contract is a ProjectContract object) 
     print(f"contract deployed at {deploy_contract}")
